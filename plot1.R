1b0fef5f12ac5ac9f773b85bcdc5be02db0951d5
nickstokes <nick.stokes@gmail.com>
2016-01-20 10:57:31 +1000

    completed plot 1

--- plot1.R
+++ plot1.R
@@ -1,5 +1,25 @@
-tempdata <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
-#tempdata$Date <- as.Date(tempdata$Date, format = "dd/mm/yyyy")
-#tempdata$Time <- strptime(tempdata$Time, format = "hh:mm:ss")
+## set up selection variables
+importFile <- "household_power_consumption.txt"
+datesNeeded <- c("1/2/2007","2/2/2007")
+
+## import csv to data frame
+tempdata <- read.csv(importFile,
+                     sep = ";",
+                     stringsAsFactors = FALSE)
+
+## filter to required dates
+tempdata <- tempdata[tempdata$Date %in% datesNeeded,]
+
+## convert other columns to numeric
 numerical_cols <- 3:9
-for(col in numerical_cols){tempdata[,col] <- as.numeric(tempdata[,col])}
+for(col in numerical_cols){
+        tempdata[,col] <- as.numeric(tempdata[,col])
+}
+
+hist(tempdata$Global_active_power, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
+dev.copy(png, "plot1.png")
+dev.off()
